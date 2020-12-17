94b24cf6a6d7f5a303add807b87c3e9658949557
Mickael Maciel <contact@onoris.fr>
2020-12-18 00:19:15 +0100

    COMMIT WEBPACK ENCORE SCSS + Fontawesome
    Installation de Webpack encore
    Ajout de Fontawesome et bulma
    Compillation de SCSS

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+/*
+Fichier principal de l'entrée Webpack Encore "app"
+On liste ici tous les fichiers CSS et JS de l'entrée
+
+Les chemins commençant par ./ partent du dossier actuel (assets)
+    .   dossier actuel
+    ..  dossier parent
+ */
+
+// Récupération du fichier SCSS de l'entrée
+require('./app.scss');
+
+// Récupération du script du menu mobile
+require('./menu.js');
